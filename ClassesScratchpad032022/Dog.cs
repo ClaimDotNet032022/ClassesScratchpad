98581187a0d0ee455d18a7cd19172cdb6e725bb9
Eric Ray <erichamion@gmail.com>
2022-04-21 19:33:02 -0500

    Add project files.

---
+++ ClassesScratchpad032022/Dog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesScratchpad032022
+{
+    public class Dog
+    {
+        public static int Population {get; private set;}
+
+        public string Color { get; }
+        public string Name { get; set; }
+        public int HeightInches { get; set; }
+        public bool IsSitting { get; private set; }
+
+
+
+        public Dog(string color, string name, int height)
+        {
+            Color = color;
+            Name = name;
+            HeightInches = height;
+            IsSitting = false;
+
+            ++Population;
+        }
+
+        public static Dog operator +(Dog dog1, Dog dog2)
+        {
+            string newName = $"{dog1.Name} and {dog2.Name}'s child";
+            int newHeight = (dog1.HeightInches + dog2.HeightInches) / 2;
+            return new Dog(dog1.Color, newName, newHeight);
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Bark! Bark!");
+        }
+
+        public void Sit()
+        {
+            IsSitting = true;
+        }
+
+        public void Stand()
+        {
+            this.IsSitting = false;
+        }
+
+        public static void DoSomethingStatic(Dog dog)
+        {
+            dog.IsSitting = false;
+        }
+    }
+}
---
+++ ClassesScratchpad032022/Program.cs
@@ -0,0 +1,92 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 3
+
+
+int n = 3;
+// { 0, 0, 0 }
+int[] arr = new int[3];
+
+Dog rex = new Dog("Red", "Rex", 24);
+//rex.Color = "Red";
+Console.WriteLine(rex.Name);
+//rex.Name = "Rex";
+//rex.HeightInches = 24;
+
+Dog spot = new Dog("Brown", "Spot", 18);
+spot.Name = "Spot";
+spot.HeightInches = 18;
+
+Console.WriteLine("Rex's color is: " + rex.Color);
+Console.WriteLine("Spot's color is: " + spot.Color);
+
+Console.WriteLine("Rex, speak!");
+rex.Speak();
+rex.Sit();
+rex.Stand();
+
+spot.Stand();
+Dog.DoSomethingStatic(spot);
+
+
+Console.WriteLine(rex.IsSitting);
+Console.WriteLine(spot.IsSitting);
+
+Dog newDog = rex + spot;
+Console.WriteLine(newDog.Name);
+
+int i = 2;
+int j = 3;
+ChangeIntValue(ref i);
+ChangeIntValue(ref j);
+j = CreateOutInt(out i);
+Console.WriteLine(i);
+
+ChangeDogName(rex);
+Console.WriteLine(rex.Name);
+
+
+
+Console.WriteLine(Dog.Population);
+Dog fido = new Dog("White", "Fido", 6);
+Console.WriteLine(Dog.Population);
+
+
+
+Cat cat = new Cat();
+Cat otherCat = new Cat("Heathcliffe");
+Cat thirdCat = new Cat();
+Cat[] allTheCats = new Cat[300];
+
+for(int catNumber = 0; catNumber < allTheCats.Length; catNumber++)
+{
+    if (catNumber % 2 == 0)
+    {
+        allTheCats[catNumber] = new Cat($"Cat number {catNumber}");
+    }
+    else
+    {
+        allTheCats[catNumber] = new Cat();
+    }
+}
+
+
+static void ChangeIntValue(ref int n)
+{
+    n = 32;
+}
+
+static int CreateOutInt(out int n)
+{
+    n = 27;
+    return 42;
+}
+
+static void ChangeDogName(Dog dog)
+{
+    dog.Name = "Changed!";
+}
+
+
+
+
